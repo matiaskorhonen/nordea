bfd022cb1fba6263ada1f9ca196de93234f017fb
Matias Korhonen <matias@kiskolabs.com>
2011-06-18 19:48:45 +0300

    Adds more documentation.

--- lib/nordea/bank.rb
+++ lib/nordea/bank.rb
@@ -16,18 +16,43 @@ module Nordea
       update_rates
     end
 
+    # Get updated rates from the Nordea server
+    #
+    # Forces an update of the exchange rates
+    #
+    # @return [Hash] rates available
     def update_rates
-      currencies.each_pair do |currency, data|
-        puts data.inspect
+      currencies(true).each_pair do |currency, data|
         rate = data[:middle_rate_for_commercial_transactions]
         add_rate("EUR", currency, rate) if known_currencies.include?(currency)
       end
+      rates
     end
 
-    def exchange(cents, from_currency, to_currency)
+    # Exchange from one currency to another
+    #
+    # @example
+    #   nordea_bank = Nordea::Bank.new
+    #   nordea_bank.exchange(100, "EUR", "USD")
+    #
+    # @param [Integer] cents the amount for the conversion in cents, or equivalent
+    # @param [String] from_currency the source currency
+    # @param [String] to_currency the target currency
+    # @return [Money] the result of the conversion
+    def exchange(cents, from_currency = "EUR", to_currency = "EUR")
       exchange_with(Money.new(cents, from_currency), to_currency)
     end
 
+    # Exchanges the given +Money+ object to a new +Money+ object in
+    # +to_currency+.
+    #
+    # @example
+    #   nordea_bank = Nordea::Bank.new
+    #   nordea_bank.exchange_with(Money.new(100, "CAD"), "USD")
+    #
+    # @param [Money] from the Money object from which to convert
+    # @param [String] to_currency the ISO code for the target currency
+    # @return [Money] the new Money object in the target currency
     def exchange_with(from, to_currency)
       rate = get_rate(from.currency, to_currency)
       unless rate
@@ -38,18 +63,31 @@ module Nordea
       Money.new((from.cents * rate).round, to_currency)
     end
 
+    # Initialize or use an existing Nordea::ExchangeRates object
+    #
+    # @return [ExchangeRates] an instance of Nordea::ExchangeRates
     def exchange_rates
       @exchange_rates ||= Nordea::ExchangeRates.new
     end
 
-    def currencies(force = true)
+    # Get the currency data from Nordea
+    #
+    # @param [Boolean] force force an update of the currency data
+    # @return [Hash] Data for all the currencies and rates from Nordea
+    def currencies(force = false)
       exchange_rates.currencies(force)
     end
     
+    # List all currencies known to the money gem
+    #
+    # @return [Array<String>] ISO currency codes
     def money_currencies
       Money::Currency::TABLE.keys.map { |c| c.to_s.upcase }
     end
 
+    # List currencies found in the Money gem *and* the Nordea currency data
+    #
+    # @return [Array<String>]  ISO currency codes
     def known_currencies
       @known_currencies = money_currencies & exchange_rates.currencies.keys
     end
