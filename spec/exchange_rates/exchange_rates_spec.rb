c371087ec96faaeb8eef59ef96d8e77c6b553605
Matias Korhonen <matias@kiskolabs.com>
2011-06-18 19:49:55 +0300

    Adds a spec for Nordea::ExchangeRates.

--- spec/exchange_rates/exchange_rates_spec.rb
+++ spec/exchange_rates/exchange_rates_spec.rb
@@ -1,7 +1,17 @@
 require "spec_helper"
 
-describe "Nordea" do
-  describe "::ExchangeRates" do
-    
+describe "Nordea::ExchangeRates" do
+  before(:each) do
+    stub_request(:get, "http://service.nordea.com/nordea-openpages/fi/lists/currency/elelctronicExchangeFI.dat").
+             to_return(:status => 200, :body => SampleData.raw)
+    @exchange_rates = Nordea::ExchangeRates.new
+  end
+
+  context "#lines" do
+    it "returns the raw data as an array of lines" do
+      lines = @exchange_rates.send(:lines)
+      lines.should be_kind_of(Array)
+      lines.count.should == 221
+    end
   end
 end
