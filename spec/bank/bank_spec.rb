fb4492fc1207561b714012d3e1623f6c2d6d38b4
Matias Korhonen <matias@kiskolabs.com>
2011-06-19 16:19:06 +0300

    Some stylistic tweaks to the specs.

--- spec/bank/bank_spec.rb
+++ spec/bank/bank_spec.rb
@@ -1,32 +1,35 @@
 require "spec_helper"
 
 describe "Nordea::Bank" do
+  let(:bank) do
+    Nordea::Bank.new
+  end
+  
   before(:each) do
     stub_request(:get, "http://service.nordea.com/nordea-openpages/fi/lists/currency/elelctronicExchangeFI.dat").
              to_return(:status => 200, :body => SampleData.raw)
-    @bank = Nordea::Bank.new
-    Money.default_bank = @bank
+    Money.default_bank = bank
   end
 
-  context "#exchange" do
+  describe "#exchange" do
     it "returns the correct exchange rates" do
-      @bank.known_currencies.each do |currency|
-        @bank.exchange(100, "EUR", currency).cents.should == (SampleData.get_rate(currency) * 100).round
+      bank.known_currencies.each do |currency|
+        bank.exchange(100, "EUR", currency).cents.should == (SampleData.get_rate(currency) * 100).round
       end
     end
   end
 
-  context "#exchange_with" do
+  describe "#exchange_with" do
     it "returns the correct exchange rates" do
-      @bank.known_currencies.each do |currency|
-        @bank.exchange_with(Money.new(100, "EUR"), currency).cents.should == (SampleData.get_rate(currency) * 100).round
+      bank.known_currencies.each do |currency|
+        bank.exchange_with(Money.new(100, "EUR"), currency).cents.should == (SampleData.get_rate(currency) * 100).round
       end
     end
   end
 
-  context "#exchange_rates" do
+  describe "#exchange_rates" do
     it "is an instance of Nordea::ExchangeRates" do
-      @bank.exchange_rates.should be_an_instance_of(Nordea::ExchangeRates)
+      bank.exchange_rates.should be_an_instance_of(Nordea::ExchangeRates)
     end
   end
 end
--- spec/exchange_rates/exchange_rates_spec.rb
+++ spec/exchange_rates/exchange_rates_spec.rb
@@ -1,40 +1,43 @@
 require "spec_helper"
 
 describe "Nordea::ExchangeRates" do
+  let(:exchange_rates) do
+    Nordea::ExchangeRates.new
+  end
+  
   before(:each) do
     stub_request(:get, "http://service.nordea.com/nordea-openpages/fi/lists/currency/elelctronicExchangeFI.dat").
              to_return(:status => 200, :body => SampleData.raw)
-    @exchange_rates = Nordea::ExchangeRates.new
   end
 
-  context "#lines" do
+  describe "#lines" do
     it "returns the raw data as an array of lines" do
-      @exchange_rates.send(:lines).should be_kind_of(Array)
+      exchange_rates.send(:lines).should be_kind_of(Array)
     end
 
     it "returns the correct number of lines" do
-      @exchange_rates.send(:lines).count.should == 221
+      exchange_rates.send(:lines).count.should == 221
     end
   end
 
-  context "#records_array" do
-    before(:each) do
-      @records = @exchange_rates.send(:records_array)
+  describe "#records_array" do
+    let(:records) do
+      exchange_rates.send(:records_array)
     end
 
     it "returns an array of hashes" do
-      @records.should be_kind_of(Array)
-      @records.each do |record|
+      records.should be_kind_of(Array)
+      records.each do |record|
         record.should be_kind_of(Hash)
       end
     end
 
     it "has the correct number of records" do
-      @records.length.should == SampleData.currencies.length
+      records.length.should == SampleData.currencies.length
     end
 
     it "has the correct data" do
-      @records.each do |record|
+      records.each do |record|
         currency = record[:currency_iso_code]
         sample = SampleData.currencies[currency]
 
@@ -45,32 +48,31 @@ describe "Nordea::ExchangeRates" do
     end
   end
 
-  context "#headers" do
-    before(:each) do
-      @headers = @exchange_rates.headers
+  describe "#headers" do
+    let(:headers) do
+      exchange_rates.headers
     end
 
     it "returns a hash" do
-      @headers.should be_kind_of(Hash)
+      headers.should be_kind_of(Hash)
     end
 
     it "has the correct data" do
-      @headers = @exchange_rates.headers
-      @headers.each_pair do |key,value|
+      headers.each_pair do |key,value|
         value.should == SampleData.headers[key]
       end
     end
   end
 
-  context "#fetch_data" do
+  describe "#fetch_data" do
     it "returns the raw data from the server if the request is successful" do
-      @exchange_rates.send(:fetch_data).should == SampleData.raw.read
+      exchange_rates.send(:fetch_data).should == SampleData.raw.read
     end
 
     it "raises an exception if the request is not successful" do
       stub_request(:get, "http://service.nordea.com/nordea-openpages/fi/lists/currency/elelctronicExchangeFI.dat").
                to_return(:status => 404, :body => SampleData.raw)
-      expect { @exchange_rates.send(:fetch_data) }.to raise_error Nordea::ServerError
+      expect { exchange_rates.send(:fetch_data) }.to raise_error Nordea::ServerError
     end
   end
 end
